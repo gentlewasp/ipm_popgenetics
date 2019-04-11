7ffcdbf144784596c7bc342987e8b74bbcc44c76
gentlewasp <shujun268@163.com>
2019-04-11 21:44:47 +1000

    add for loop.

--- assignPOP_v1.R
+++ assignPOP_v1.R
@@ -7,22 +7,34 @@ library(genepopedit)
 
 work_dir <- "/Users/macbook2017/Desktop/dapc/" 
 setwd(work_dir)
-all.gen  = "ec.140.2.500.6437.neutral_3pop.gen"
+all.gen  = "ec.287.mac2.thin500.7172.gen.txt"
 all.renamed.gen="all.renamed.gen"
-
-genepop_ID(genepop=all.gen, path=paste0(work_dir, all.renamed.gen))  ##BJYQ01 > BJYQ_01
-
+species = "ec.test"
+genepop_ID(genepop=all.gen, path=paste0(work_dir, all.renamed.gen)) 
 PopNames.all <- genepop_detective(all.renamed.gen, variable="Pops")
 PopCounts.all <- genepop_detective(all.renamed.gen, variable="PopNum")
 PopCounts.all
-all_ref <- read.Genepop(all.renamed.gen, pop.names=PopNames.all)
+for (i in PopNames.all) {
+  pop.sublist <- c("SCLS", i)
+  pop.sublist
+  pop.sublistname <- paste(pop.sublist, collapse ="_", sep=".")
+  pop.sublistname
+  subset_genepop(genepop= all.renamed.gen, keep = TRUE, 
+                 spop = pop.sublist, 
+                 path = paste0(work_dir, paste(pop.sublistname, ".gen", sep="")))
 
-#all_inc <- read.Genepop( "nokin_allq9_inc.txt")
-#all_ref_rd <- reduce.allele(all_ref, p = 0.95)
-
-assign_trial = assign.MC(all_ref, train.inds=c(0.9), train.loci=c(0.8), 
-                         loci.sample="fst", iterations=100, dir="mc_randomForest1/", model="randomForest" )   
-
-accuMC200 <- accuracy.MC(dir = "mc_randomForest1/")
-accuracy.plot(accuMC200, pop = PopNames.all) 
+  used.PopNames <- genepop_detective(paste(pop.sublistname, ".gen", sep=""), variable="Pops")
+  used.PopNames
+  all_ref <- read.Genepop(paste(pop.sublistname, ".gen", sep=""), pop.names=used.PopNames)
+  #all_inc <- read.Genepop( "nokin_allq9_inc.txt")
+  #all_ref_rd <- reduce.allele(all_ref, p = 0.95)
+  assign_trial = assign.MC(all_ref, train.inds=c(0.9), train.loci=c(0.8), 
+                         loci.sample="fst", iterations=5, dir = paste(species, pop.sublistname, ".mc_svm/", sep=""), model="svm" )   
+  accuMC200 <- accuracy.MC(dir = paste(species, pop.sublistname, ".mc_svm/", sep=""))
+  pdf(file=paste(species, pop.sublistname, ".assignpop.mc.plot.pdf", sep=""))
+  accuracy.plot(accuMC200, pop = used.PopNames) 
+  dev.off()
+  count = count + 1
+  }
+file.remove("all.renamed.gen")
 
