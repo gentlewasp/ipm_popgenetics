09769dcbf28e88879bfb5fa182034706c97c8b67
gentlewasp <shujun268@163.com>
2019-04-14 18:27:25 +1000

    define model variable, full scripts

--- assignPOP_v2.R
+++ assignPOP_v2.R
@@ -7,7 +7,7 @@ library(genepopedit)
 
 work_dir <- "/Users/macbook2017/Desktop/dapc/" 
 setwd(work_dir)
-all.gen  = "px_ddRAD_2018_432_6230_mac3_thin500-twogroups.gen"
+all.gen  = "px_ddRAD_2018_432_6230_mac3_thin500-twogroups2.gen"
 all.renamed.gen="all.renamed.gen"
 species = "px_2018"
 genepop_ID(genepop=all.gen, path=paste0(work_dir, all.renamed.gen)) 
@@ -23,6 +23,7 @@ SampleIDs <- genepop_detective(paste(pop.sublistname, ".pop.gen", sep=""), varia
 SampleIDs
 subid <- c("GD_GA03", "GD_GA19", "GD_GA11", "GD_GA09", "GD_GA01", "GD_GA10", "GD_GA08", "GD_GA12", "GD_GA20", "GD_GA14", "GD_GA02", "GD_GA07","GD_GA13",
            "GD_SZ13", "GD_SZ18",  "GD_SZ20", "GD_SZ10", "GD_SZ06", "GD_SZ19", "GD_SZ14", 
+           "GD_GB02", "GD_GC13", "GD_GC12", "GD_GC07", "GD_GC02",
            "YN_KM04", "YN_KM13", "YN_KM08", "YN_KM14", "YN_KM17", "YN_KM07", "YN_KM19", "YN_KM15",
            "YN_YX08", "YN_YX19", "YN_YX02", "YN_YX06", "YN_YX01", "YN_YX14") #"GXNN_01", "GXNN_06", "GDGC_01", "GDGC_13","YNYX_13", "YNYX_14", "YNKM_13", "YNKM_17"
 subset_genepop_individual(genepop= paste(pop.sublistname, ".pop.gen", sep=""), 
@@ -37,6 +38,8 @@ used.PopCounts
 all_ref <- read.Genepop(paste(pop.sublistname, ".gen", sep=""), pop.names=used.PopNames)
 #all_inc <- read.Genepop( "nokin_allq9_inc.txt")
 #all_ref_rd <- reduce.allele(all_ref, p = 0.95)
+
+## MCMC ##
 model.set = "svm"  #"lda", "svm", "naiveBayes", "tree", and "randomForest". Default is "svm"(support vector machine).
 assign_trial = assign.MC(all_ref, train.inds=c(0.92), train.loci=c(0.8), 
                          loci.sample="fst", iterations=50, 
@@ -47,6 +50,7 @@ pdf(file=paste(species, pop.sublistname, model.set, "assignpop.plot.pdf", sep=".
 accuracy.plot(accuMC, pop = c("all", used.PopNames))
 dev.off()
 
+## Kfold validation ##
 assign.kfold( all_ref, k.fold=c(2,3), train.loci=c(1), 
               loci.sample="random", model=model.set, 
               dir = paste(species, pop.sublistname, "kfold_result/", sep="."))
@@ -61,8 +65,10 @@ check.loci(dir = paste(species, pop.sublistname, model.set, "result/", sep="."),
 file.remove(paste(pop.sublistname, ".gen", sep=""))
 #file.remove(paste(pop.sublistname, ".pop.gen", sep=""))
 #file.remove("all.renamed.gen")
+
+##assigntest##
 PopNames.all
-pop.sublist.test <- c("FJFZ", "SCPZ", "HNCS", "JXNA", "NMAL", "BJYA", "HBZA")
+pop.sublist.test <- c("GDHK", "GDG",  "GDGC", "GDNN" , "FJFZ", "YNDH", "YNPZ", "SCPZ", "HNCS", "JXNA", "JXNB", "JXNC", "ZJNB", "SCCD", "SCGY", "NMAL", "BJYA", "HBZA")
 pop.sublistname.test <- paste(pop.sublist.test, collapse ="_", sep=".")
 subset_genepop(genepop= all.renamed.gen, keep = TRUE, 
                spop = pop.sublist.test, 
