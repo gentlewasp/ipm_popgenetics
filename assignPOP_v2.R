8149bf86e865c21e1c7df4f41fa69317fdbb21cd
gentlewasp <shujun268@163.com>
2019-04-14 12:29:55 +1000

    added kfold, assign.X

--- assignPOP_v2.R
+++ assignPOP_v2.R
@@ -9,11 +9,10 @@ work_dir <- "/Users/macbook2017/Desktop/dapc/"
 setwd(work_dir)
 all.gen  = "px_ddRAD_2018_432_6230_mac3_thin500-twogroups.gen"
 all.renamed.gen="all.renamed.gen"
-species = "px_2018."
+species = "px_2018"
 genepop_ID(genepop=all.gen, path=paste0(work_dir, all.renamed.gen)) 
 PopNames.all <- genepop_detective(all.renamed.gen, variable="Pops")
 PopCounts.all <- genepop_detective(all.renamed.gen, variable="PopNum")
-PopNames.all
 PopCounts.all
 pop.sublist <- c("GD", "YN")
 pop.sublistname <- paste(pop.sublist, collapse ="_", sep=".")
@@ -22,7 +21,10 @@ subset_genepop(genepop= all.renamed.gen, keep = TRUE,
                  path = paste0(work_dir, paste(pop.sublistname, ".pop.gen", sep="")))
 SampleIDs <- genepop_detective(paste(pop.sublistname, ".pop.gen", sep=""), variable="Inds")
 SampleIDs
-subid <- c() #"GXNN_01", "GXNN_06", "GDGC_01", "GDGC_13","YNYX_13", "YNYX_14", "YNKM_13", "YNKM_17"
+subid <- c("GD_GA03", "GD_GA19", "GD_GA11", "GD_GA09", "GD_GA01", "GD_GA10", "GD_GA08", "GD_GA12", "GD_GA20", "GD_GA14", "GD_GA02", "GD_GA07","GD_GA13",
+           "GD_SZ13", "GD_SZ18",  "GD_SZ20", "GD_SZ10", "GD_SZ06", "GD_SZ19", "GD_SZ14", 
+           "YN_KM04", "YN_KM13", "YN_KM08", "YN_KM14", "YN_KM17", "YN_KM07", "YN_KM19", "YN_KM15",
+           "YN_YX08", "YN_YX19", "YN_YX02", "YN_YX06", "YN_YX01", "YN_YX14") #"GXNN_01", "GXNN_06", "GDGC_01", "GDGC_13","YNYX_13", "YNYX_14", "YNKM_13", "YNKM_17"
 subset_genepop_individual(genepop= paste(pop.sublistname, ".pop.gen", sep=""), 
                           indiv = subid, 
                           keep = FALSE,  
@@ -35,14 +37,26 @@ used.PopCounts
 all_ref <- read.Genepop(paste(pop.sublistname, ".gen", sep=""), pop.names=used.PopNames)
 #all_inc <- read.Genepop( "nokin_allq9_inc.txt")
 #all_ref_rd <- reduce.allele(all_ref, p = 0.95)
+model.set = "svm"  #"lda", "svm", "naiveBayes", "tree", and "randomForest". Default is "svm"(support vector machine).
 assign_trial = assign.MC(all_ref, train.inds=c(0.92), train.loci=c(0.8), 
                          loci.sample="fst", iterations=50, 
-                         dir = paste(species, pop.sublistname, ".mc_svm/", sep=""), 
-                         model="svm" )   
-accuMC <- accuracy.MC(dir = paste(species, pop.sublistname, ".mc_svm/", sep=""))
-pdf(file=paste(species, pop.sublistname, ".assignpop.mc.plot.pdf", sep=""))
+                         dir = paste(species, pop.sublistname, model.set, "result/", sep="."), 
+                         model = model.set )   
+accuMC <- accuracy.MC(dir = paste(species, pop.sublistname, model.set, "result/", sep="."))
+pdf(file=paste(species, pop.sublistname, model.set, "assignpop.plot.pdf", sep="."))
 accuracy.plot(accuMC, pop = c("all", used.PopNames))
 dev.off()
+
+assign.kfold( all_ref, k.fold=c(2,3), train.loci=c(1), 
+              loci.sample="random", model=model.set, 
+              dir = paste(species, pop.sublistname, "kfold_result/", sep="."))
+accuKF <- accuracy.kfold(dir = paste(species, pop.sublistname, "kfold_result/", sep=".")) #Use this function for K-fold cross-validation results
+pdf(file=paste(species, pop.sublistname, "kfold.membership.plot.pdf", sep="."), width=30, height=8)
+membership.plot(dir = paste(species, pop.sublistname, "kfold_result/", sep=".")) 
+dev.off()
+
+check.loci(dir = paste(species, pop.sublistname, model.set, "result/", sep="."), top.loci = 1000)
+
 #used.PopNames
 file.remove(paste(pop.sublistname, ".gen", sep=""))
 #file.remove(paste(pop.sublistname, ".pop.gen", sep=""))
@@ -55,6 +69,8 @@ subset_genepop(genepop= all.renamed.gen, keep = TRUE,
                path = paste0(work_dir, paste(pop.sublistname.test, ".pop.gen", sep="")))
 used.PopNames.test <- genepop_detective(paste(pop.sublistname.test, ".pop.gen", sep=""), variable="Pops")
 all_inc <- read.Genepop(paste(pop.sublistname.test, ".pop.gen", sep=""), pop.names=used.PopNames.test)
-pdf(file=paste(pop.sublistname, "(ref)", pop.sublistname.test, ".assignpop.plot.pdf",sep="."))
-assign.X(x1=all_ref, x2=all_inc, dir=paste(pop.sublistname, "(ref)", pop.sublistname.test, "svm/",sep="."), model="svm",mplot=TRUE)
+pdf(file=paste(pop.sublistname, "(ref)", pop.sublistname.test, model.set, "assignpop.plot.pdf",sep="."))
+assign.X(x1=all_ref, x2=all_inc, 
+         dir=paste(pop.sublistname, "(ref)", pop.sublistname.test, model.set, "result/", sep="."), 
+         model = model.set, mplot=TRUE)
 dev.off()
